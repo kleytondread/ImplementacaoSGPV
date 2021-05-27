64a0e1a9e4a5c865d5e28b5bfb714eed612ed874
Kleyton Clementino <kleytondread@gmail.com>
2021-05-27 19:29:56 -0300

    Add files via upload
    commit inicial

---
+++ SubindoNivelProjeto/Common/Extentions/ServiceProviderExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Common
+{
+    public static class ServiceProviderExtension
+    {
+        public static T GetService<T>(this IServiceProvider serviceProvider)
+        {
+            return (T)serviceProvider.GetService(typeof(T));
+        }
+    }
+}
